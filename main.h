3e791d60920ba8a868a057c7d137e46b06ecf7b7
Ace <AceQuantumBit@gmail.com>
2023-11-02 15:47:28 -0500

    switched the field declaration of format struct

--- main.h
+++ main.h
@@ -10,11 +10,11 @@
 
 typedef struct format
 {
-    /*Field to hold format specifier*/
-    char *op;
-
     /*Field to hold operation*/
     int (*f)(va_list op_list);
+
+    /*Field to hold format specifier*/
+    char *op;
 
 } form;
 
