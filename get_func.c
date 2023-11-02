7a70514198ea6b47a71c0e9bea0475aa8ce7fb31
Ace <AceQuantumBit@gmail.com>
2023-11-02 16:05:54 -0500

    Moved squiggly bracket

--- get_func.c
+++ get_func.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 int (*get_func(char format))(va_list args)
+{
 form forms[] = {
     {'c', print_char},
     {'s', print_str},
     {'d', print_int},
     {NULL, NULL}};
 
-{
+
     int i = 0;
 
     while (form[i] != NULL)
