121b438f007b915699d69bc9727669ac83d166df
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-04 22:17:20 +0000

    rm skip var

--- printf.c
+++ printf.c
@@ -9,9 +9,6 @@ int _printf(const char *format, ...)
 	/**int i = 0;**/
 	int tracker = 0;
 
-	/**Flag to skip next character after "%"*/
-	int skip = 0;
-
 	va_list args;
 	va_start(args, format);
 
@@ -23,15 +20,10 @@ int _printf(const char *format, ...)
 
 	while (*format != '\0')
 	{
-		if (skip)
-		{
-			skip = 0; /** Reset the flag and do not advance*/
-		}
-		else if (*format == '%')
+		if (*format == '%')
 		{
 			format++;
 			tracker += (get_func(*format)(args));
-			skip = 1; /** Set flag to skip character */
 		}
 		else
 		{
