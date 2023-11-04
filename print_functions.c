1e260908a5c957a87ec1d1cd391e4c08e08c8e12
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-11-04 20:59:56 +0000

    split var declarations for compiler

--- print_functions.c
+++ print_functions.c
@@ -26,7 +26,8 @@ int print_char(va_list args)
 
 int print_str(va_list args)
 {
-    char *str = va_arg(args, char *);
+    char *str;
+    str = va_arg(args, char *);
 
     /**Null check for variable assignment*/
 
@@ -35,7 +36,8 @@ int print_str(va_list args)
         str = "(nil)";
     }
 
-    int printed = fputs(str, stdout);
+    int printed;
+    printed = fputs(str, stdout);
 
     /**Return output on success, return 0 on fail*/
 
